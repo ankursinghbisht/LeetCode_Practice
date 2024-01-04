12e1dcf1f8202318acbc8161889472111fcaea16
Ankur Singh Bisht <ankursinghbisht007@gmail.com>
2024-01-04 15:51:50 +0530

    Replacing elements of array according to operations declared (C++)

---
+++ C++/2295. Replace Elements in an Array.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations)
+    {
+        unordered_map<int, int> mp;
+        for (int i = 0;i < nums.size();i++)
+            mp[nums[i]] = i;
+
+        for (int i = 0;i < operations.size();i++)
+        {
+            int pos = mp[operations[i][0]];
+            nums[pos] = operations[i][1];
+            mp[operations[i][1]] = pos;
+        }
+        return nums;
+    }
+};
